888c8121cec88e8a3762a955a85810f1e5e00add
Eric <eweast@gmail.com>
2014-08-06 18:30:16 -0500

    Update

--- BenEncoder.py
+++ BenEncoder.py
@@ -1,24 +1,29 @@
 __author__ = 'eric.weast'
 
-import collections
+
+class EncodingError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return repr(self.msg)
 
 
-class Bencode():
-    def __init__(self):
-        self.__coded_byte_str = b''
-        self.encoding = 'utf-8'
-
-        self.encode_options = {
-            str: self.__encode_str,
-            bytes: self.__encode_byte_str,
-            int: self.__encode_int,
-            list: self.__encode_list,
-            dict: self.__encode_dict
-        }
+class BenEncoder():
+    def __init__(self, encoding='utf-8', strict=True):
+        self.coded_bytes = b''
+        self.encoding = encoding
+        self.strict = strict
+        self.errors = 0
 
     def encode(self, obj):
+        self.__reset()
         self.__select_encoder(obj)
-        return self.__coded_byte_str
+        return self.coded_bytes
+
+    def __reset(self):
+        self.coded_bytes = b''
+        self.errors = 0
 
     def __select_encoder(self, obj):
         """Calls the appropriate function to encode the passed object (obj)."""
@@ -33,7 +38,12 @@ class Bencode():
         elif isinstance(obj, list):
             self.__encode_list(obj)
         else:
-            print('Error, encoder not found.')
+            if self.strict:
+                self.coded_bytes = b''
+                raise EncodingError("Unable to encode object: " + obj.__repr__())
+            else:
+                print("Unable to encode object: " + str(obj))
+                self.errors += 1
 
     def __encode_str(self, s: str):
         """Converts the input string to bytes and passes it the __encode_byte_str function for encoding."""
@@ -43,33 +53,33 @@ class Bencode():
     def __encode_byte_str(self, b: bytes):
         """Ben-encodes string from bytes."""
         length = len(b)
-        self.__coded_byte_str += bytes(str(length), 'utf-8') + b':'
-        self.__coded_byte_str += b
+        self.coded_bytes += bytes(str(length), 'utf-8') + b':'
+        self.coded_bytes += b
 
     def __encode_int(self, i: int):
         """Ben-encodes integer from int."""
-        self.__coded_byte_str += b'i'
-        self.__coded_byte_str += bytes(str(i), 'utf-8')
-        self.__coded_byte_str += b'e'
+        self.coded_bytes += b'i'
+        self.coded_bytes += bytes(str(i), 'utf-8')
+        self.coded_bytes += b'e'
 
     def __encode_list(self, l: list):
         """Ben-encodes list from list."""
-        self.__coded_byte_str += b'l'
+        self.coded_bytes += b'l'
         for i in l:
             self.__select_encoder(i)
-        self.__coded_byte_str += b'e'
+        self.coded_bytes += b'e'
 
     def __encode_dict(self, d: dict):
         """Ben-encodes dictionary from dict."""
-        self.__coded_byte_str += b'd'
+        self.coded_bytes += b'd'
         for k in d:
             self.__select_encoder(k)
             self.__select_encoder(d[k])
-        self.__coded_byte_str += b'e'
+        self.coded_bytes += b'e'
 
 
 mydict = {'keyA': [142, '2ndListitemInA', {'innerKey2': 'innerValue2'} ], b'keyB': b'valueBstr'}
-ben = Bencode()
+ben = BenEncoder()
 bs = ben.encode(mydict)
 print(bs)
 
