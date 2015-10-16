a9c145bd087c61dd8fb28a9dfad46d085c8b8290
Eric <eweast@gmail.com>
2015-10-16 18:37:17 -0500

    Corrected url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='bencodepy',
     version='0.9.5',
     packages=['bencodepy'],
-    url='https://github.com/eweast/bencodepy_opti',
+    url='https://github.com/eweast/bencodepy',
     license='GPLv2',
     author='Eric Weast',
     author_email='eweast@hotmail.com',
