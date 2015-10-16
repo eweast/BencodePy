ccaab7fa1b7de04a1d25d6788d5c8589cf865075
eric.weast <eweast@gmail.com>
2015-10-16 16:27:54 -0500

    setup update

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from distutils.core import setup
-import os
-
-description = ''
-if os.path.isfile('README.md'):
-    with open('README.md', 'r') as f:
-        description = f.read()
 
 setup(
     name='bencodepy',
@@ -14,6 +8,5 @@ setup(
     license='GPLv2',
     author='Eric Weast',
     author_email='eweast@hotmail.com',
-    description='Bencode encoder/decoder written in Python 3 under the GPLv2.',
-    long_description=description
+    description='Bencode encoder/decoder written in Python 3 under the GPLv2.'
 )
